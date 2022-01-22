8661f1551fd2367200231872a55b5d5c72a37f33
Juani Greco <juanigreco22@gmail.com>
2022-01-22 16:58:37 -0300

    2do entregable, navbar. Juani Greco

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css';
+
+export const NavBar = () => {
+
+    return (
+            <header className="header">
+                <h2>LOGO</h2>
+
+            <nav className="header-nav">
+                <p className="header-link">Inicio</p>
+                <p className="header-link">Catálogo</p>
+                <p className="header-link">Carrito</p>
+            </nav>
+        </header>
+    )
+
+}
